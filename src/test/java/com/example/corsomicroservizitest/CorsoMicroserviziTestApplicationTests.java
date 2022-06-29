4ce45ba215f73e4d550f6e917861e069124e0b62
ema-2000 <emanueleficarola@hotmail.it>
2022-06-29 13:04:27 +0200

    Primo commit

---
+++ src/test/java/com/example/corsomicroservizitest/CorsoMicroserviziTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.corsomicroservizitest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CorsoMicroserviziTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
